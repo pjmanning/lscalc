02eca4220704683b7c040c36e11fb8156feb8f54
PJ Manning <phil.manning11@gmail.com>
2023-03-22 20:04:39 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="background" class="h-screen w-screen flex">
+    <div class="bg-white bg-opacity-50 h-4/5 w-4/5 m-auto rounded-lg">
+      <h1 class="text-center pt-10 text-4xl">Lemon Squeezy Calculator</h1>
+      <!-- <p class="text-center pt-10">Able is a web application that allows you to create and manage your own projects.</p>
+      <p class="text-center">To get started, click the button below.</p>
+      <div class="text-center pt-10">
+        <RouterLink to="/projects">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get Started</button>
+        </RouterLink>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#background {
+  background-image: url('able-lavender-dinosaur.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
